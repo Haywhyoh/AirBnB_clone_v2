be3dc0640755134083cfa832564b9996b21632a5
Adedayo Ayomide <adefeyisayo998@gmail.com>
2022-09-15 15:04:29 +0100

    first attempt for the test sql

---
+++ setup_mysql_test.sql
@@ -0,0 +1,10 @@
+-- create database called hbnb_test_db
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+-- CREATE A USES IF NOT EXIST
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+-- grant all privileges to hbnb_test
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test_dev'@'localhost';
+-- grant select privileges to hbnb_test_dev
+GRANT SELECT ON performance_schema.* TO 'hbnb_test_dev'@'localhost';
+-- flush privileges
+FLUSH PRIVILEGES;
