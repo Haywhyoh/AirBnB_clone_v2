952265c43bafe24e99511930ba245a3d99c9826f
Adedayo Ayomide <adefeyisayo998@gmail.com>
2022-09-15 15:07:20 +0100

    flush the privvileges toilet

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -4,6 +4,7 @@ CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 -- grant all privileges to hbnb_test
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test_dev'@'localhost';
+FLUSH PRIVILEGES;
 -- grant select privileges to hbnb_test_dev
 GRANT SELECT ON performance_schema.* TO 'hbnb_test_dev'@'localhost';
 -- flush privileges
