d52626c491539cc32638ceba75167d6486cf9fac
Adedayo Ayomide <adefeyisayo998@gmail.com>
2022-09-15 13:56:11 +0100

    created sql server and user

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,9 @@
+-- create database hbnb_dev_db
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+-- create a new user if not exists
+CREATE USER 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+-- grant all privilege
+GRANT ALL ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+-- grant SELECT privileges on performance_schema
+GRANT SELECT ON performance_schema.*
+TO 'hbnb_dev'@'localhost';
