3ffcc80a2752a82093a34219c815866328a9bbdc
Adedayo Ayomide <adefeyisayo998@gmail.com>
2022-09-21 23:11:20 +0100

    fixed city grand theft auto

--- models/city.py
+++ models/city.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
+""" City Module for HBNB project """
 import os
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, ForeignKey
+from models.state import State
 from sqlalchemy.orm import relationship
-from models.place import Place, place_amenity
 
 
-class Amenity(BaseModel, Base):
-    """ Amenities different places have """
-    __tablename__ = 'amenities'
+class City(BaseModel, Base):
+    """ The city class, contains state ID and name """
+    __tablename__ = 'cities'
     if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        name = Column(String(1024), nullable=False)
-        place_amenities = relationship(
-            "Place", secondary=place_amenity, viewonly=False)
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        name = Column(String(128), nullable=False)
+        places = relationship(
+            "Place", cascade="all, delete, delete-orphan", backref="cities")
     else:
         name = ""
+        state_id = ""
