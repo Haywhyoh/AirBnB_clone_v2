ef6649ea825ebbf44d25ae8e4b2b407eeecfdb9c
Adedayo Ayomide <adefeyisayo998@gmail.com>
2022-09-29 14:17:44 +0100

    added more space

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,6 +4,7 @@ Fabric script based on the file 1-pack_web_static.py that distributes an
 archive to the web servers
 """
 
+
 from fabric.api import run, put, env
 import os.path as file_path
 env.username = "ubuntu"
