0889d2408703e81d1eefd9f040eedadeec1bf3b3
Adedayo Ayomide <adefeyisayo998@gmail.com>
2022-09-29 10:45:25 +0100

    I did sudo tee for the index.html

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,7 +3,7 @@ sudo apt -y install update
 sudo apt -y install nginx
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
-sudo echo "Thiis is a test text" > /data/web_static/releases/test/index.html
+echo "This is a test text" > sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -hR ubuntu:ubuntu /data/
 #sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
