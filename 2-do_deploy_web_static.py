d1ab41eac42894380099895d459d52a372efd27c
Adedayo Ayomide <adefeyisayo998@gmail.com>
2022-09-29 14:22:33 +0100

    I just tire to code

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,32 +1,40 @@
 #!/usr/bin/python3
-"""
-Fabric script based on the file 1-pack_web_static.py that distributes an
-archive to the web servers
-"""
+""" Function that compress a folder """
+from datetime import datetime
+from fabric.api import *
+import shlex
+import os
 
 
-from fabric.api import run, put, env
-import os.path as file_path
-env.username = "ubuntu"
-env.hosts['100.24.210.123', '3.235.24.154']
+env.hosts = ['35.231.33.237', '34.74.155.163']
+env.user = "ubuntu"
 
 
-def de_deploy(archive_path):
-    """distributes an archive to the web servers"""
+def do_deploy(archive_path):
+    """ Deploys """
+    if not os.path.exists(archive_path):
+        return False
     try:
-        path = '/data/web_static/releases/'
-        archive_file = archive_path.split('/')[-1]
-        file_name = archive_file.split('.')[0]
-        if not file_path.exists(archive_path):
-            return False
-        put(archive_path, '/tmp/')
-        run("mkdir -p {}{}/".format(path, file_name))
-        run("tar -xzf /tmp/{} -C {}{}/".format(archive_file, path, file_name))
-        run("rm -f /tmp/{}".format(archive_file))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
-        run('rm -rf {}{}/web_static'.format(path, no_ext))
-        run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        name = archive_path.replace('/', ' ')
+        name = shlex.split(name)
+        name = name[-1]
+
+        wname = name.replace('.', ' ')
+        wname = shlex.split(wname)
+        wname = wname[0]
+
+        releases_path = "/data/web_static/releases/{}/".format(wname)
+        tmp_path = "/tmp/{}".format(name)
+
+        put(archive_path, "/tmp/")
+        run("mkdir -p {}".format(releases_path))
+        run("tar -xzf {} -C {}".format(tmp_path, releases_path))
+        run("rm {}".format(tmp_path))
+        run("mv {}web_static/* {}".format(releases_path, releases_path))
+        run("rm -rf {}web_static".format(releases_path))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {} /data/web_static/current".format(releases_path))
+        print("New version deployed!")
         return True
-    except FileExistsError:
+    except:
         return False
