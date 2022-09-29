82de694185981ddb2febf6c6afeb19c9ff8d03f7
Adedayo Ayomide <adefeyisayo998@gmail.com>
2022-09-29 12:35:42 +0100

    did the conversion and version storing

---
+++ 1-pack_web_static.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Fabric script that generates a tgz archive from the contents of the web_static
+folder of the AirBnB Clone repo
+"""
+
+
+from fabric.api import local
+from datetime import datetime
+import os.path as file_path
+def do_pack():
+    """generates a tgz archive"""
+    try:
+        time = datetime.now().strftime("%Y%m%d%H%M%S")
+        archive_file = "versions/web_static_{}.tgz".format(time)
+        if not file_path.exists("versions"):
+            local('mkdir -p versions')
+        #creatd the tgz with tar command
+        #local("tar -cvzf {} webstatic".format(archive_file))
+        local("tar -cvzf {} web_static".format(archive_file))
+        return archive_file
+    except:
+        return None
