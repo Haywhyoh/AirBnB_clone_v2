89f9ae960094225cefdeca8f6cad9807b11a9a38
Adedayo Ayomide <adefeyisayo998@gmail.com>
2022-09-29 10:58:41 +0100

    apt-get upgrade used

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # sets up the web servers for the deployment of web_static
-sudo apt -y install update
-sudo apt -y install nginx
+sudo apt-get -y install update
+sudo apt-get -y install nginx
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
 echo "This is a test text" > sudo tee /data/web_static/releases/test/index.html
