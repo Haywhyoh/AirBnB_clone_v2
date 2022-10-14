c5e8ad0fca0eac331a816aecbb21b655fa899053
Adedayo Ayomide <adefeyisayo998@gmail.com>
2022-10-14 09:00:30 +0100

    made some few changes

--- web_flask/0-hello_route.py, __init__.py
+++ web_flask/0-hello_route.py, __init__.py
@@ -1,3 +1,4 @@
+#!usr/bin/python3
 """This is a script to create a basic flask app"""
 from flask import Flask
 
@@ -5,7 +6,8 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes= False)
 def display_hello():
+    """Displays 'Hello HBNB!'"""
     return (“Hello HBNB!”)
 
 if __name__ = "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0")
