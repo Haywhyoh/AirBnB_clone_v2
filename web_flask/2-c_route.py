ab62d715953c2796ca5018b28f53c9f1a3aad12d
Adedayo Ayomide <adefeyisayo998@gmail.com>
2022-10-14 09:55:57 +0100

    tried another approach

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,31 +1,25 @@
 #!/usr/bin/python3
-"""Starts Flask web app
-Routes:
-    / - display "Hello HBNB!"
-    /hbnb - display "HBNB"
-"""
+'''crates a flask web application'''
 from flask import Flask
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-@app.route('/', strict_slashes=False)
-def display_hello():
-    """prints Hello HBNB"""
-    return "Hello HBNB!"
+@app.route('/')
+def hello():
+    return('Hello HBNB!')
 
 
-@app.route('/hbnb', strict_slashes=False)
-def display_hbnb():
-    """prints HBNB"""
-    return "HBNB"
+@app.route('/hbnb')
+def hbnb():
+    return('HBNB')
 
 
-@app.route('/c_text/<string:text>', strict_slashes=False)
-def c_text(text):
-    """prints C followed by <text> content"""
+@app.route('/c/<text>')
+def c_is_fun(text):
     return('C %s' % text.replace("_", " "))
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host='0.0.0.0')
