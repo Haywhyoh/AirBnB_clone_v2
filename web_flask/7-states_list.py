f3e57f4dedacfbff851727fe5bef54d143adad88
Adedayo Ayomide <adefeyisayo998@gmail.com>
2022-10-15 14:10:41 +0100

    some pycode styke fix

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,29 +1,21 @@
 #!/usr/bin/python3
-"""Starts a flask app
-    listens to 0.0.0.0:5000
-    
-"""
+'''crates a flask web application'''
+from flask import Flask, render_template
 from models import storage
-from flask import Flask
-from flask import render_template
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-@app.route("/states_list", strict_slashes=False)
-def states_list():
-    """Displays an HTML page with a list of all State objects in DBStorage.
-    States are sorted by name.
-    """
-    states = storage.all("State")
-    return render_template("7-states_list.html", states=states)
+@app.route('/states_list')
+def display_states():
+    return render_template('7-states_list.html', storage=storage.all('State'))
 
 
 @app.teardown_appcontext
-def teardown(exc):
-    """Remove the current SQLAlchemy session."""
+def close(exception):
     storage.close()
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host='0.0.0.0')
