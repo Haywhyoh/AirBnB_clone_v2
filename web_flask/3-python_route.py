159e616ecd0d4903186f144b6b61cf96fc7a4db8
Adedayo Ayomide <adefeyisayo998@gmail.com>
2022-10-16 23:29:04 +0100

    python reloaded

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,33 +1,42 @@
 #!/usr/bin/python3
-'''crates a flask web application'''
+"""Starts Flask web app
+Routes:
+    / - display "Hello HBNB!"
+    /hbnb - display "HBNB"
+    /c/<text> - display "C <text>"
+    /python/<text> - display "Python is cool"
+"""
 from flask import Flask
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
-@app.route('/')
-def hello():
-    return('Hello HBNB!')
+@app.route('/', strict_slashes=False)
+def hbnb_route():
+    """prints Hello HBNB"""
+    return "Hello HBNB!"
 
 
-@app.route('/hbnb')
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    return('HBNB')
+    """prints HBNB"""
+    return "HBNB"
 
 
-@app.route('/c/<text>')
-def c_is_fun(text):
-    return('C %s' % text.replace("_", " "))
+@app.route('/c/<string:text>', strict_slashes=False)
+def c_text(text):
+    """prints C followed by <text> content"""
+    text = text.replace("_", " ")
+    return "C %s" % text
 
 
-@app.route('/python')
-@app.route('/python/<string:text>')
-def python_is_fun(text='is cool'):
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<string:text>', strict_slashes=False)
+def python_text(text="is cool"):
     """prints Python is cool"""
     text = text.replace("_", " ")
     return "Python %s" % text
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host="0.0.0.0")
