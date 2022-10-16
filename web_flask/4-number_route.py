ba59f7b3ddfba6432325f114629539ef0067476c
Adedayo Ayomide <adefeyisayo998@gmail.com>
2022-10-16 23:29:45 +0100

    massive inclusive update

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,38 +1,49 @@
 #!/usr/bin/python3
-'''crates a flask web application'''
+"""Starts Flask web app
+Routes:
+    / - display "Hello HBNB!"
+    /hbnb - display "HBNB"
+    /c/<text> - display "C <text>"
+    /python/<text> - display "Python is cool"
+    /number/<n> - display n if integer
+"""
 from flask import Flask
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
-@app.route('/')
-def hello():
-    return ('Hello HBNB')
+@app.route('/', strict_slashes=False)
+def hbnb_route():
+    """prints Hello HBNB"""
+    return "Hello HBNB!"
 
 
-@app.route('/hbnb')
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    return ('HBNB')
+    """prints HBNB"""
+    return "HBNB"
 
 
-@app.route('/c/<string:text>')
-def c_is_fun(text):
-    return "C %s" % text.replace('_', ' ')
+@app.route('/c/<string:text>', strict_slashes=False)
+def c_text(text):
+    """prints C followed by <text> content"""
+    text = text.replace("_", " ")
+    return "C %s" % text
 
 
-@app.route('/python')
-@app.route('/python/<string:text>')
-def python_is_fun(text='is cool'):
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<string:text>', strict_slashes=False)
+def python_text(text="is cool"):
     """prints Python is cool"""
     text = text.replace("_", " ")
     return "Python %s" % text
 
 
-@app.route('/number/<int:n>')
-def num(n):
-    return ("%d is a number" % n)
+@app.route('/number/<int:n>', strict_slashes=False)
+def number_n(n):
+    """display n if integer"""
+    return "%i is a number" % n
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host="0.0.0.0")
--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,21 +1,29 @@
 #!/usr/bin/python3
-'''crates a flask web application'''
-from flask import Flask, render_template
+"""Starts a flask app
+    listens to 0.0.0.0:5000
+    
+"""
 from models import storage
+from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
-@app.route('/states_list')
-def display_states():
-    return render_template('7-states_list.html', storage=storage.all('State'))
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    """Displays an HTML page with a list of all State objects in DBStorage.
+    States are sorted by name.
+    """
+    states = storage.all("State")
+    return render_template("7-states_list.html", states=states)
 
 
 @app.teardown_appcontext
-def close(exception):
+def teardown(exc):
+    """Remove the current SQLAlchemy session."""
     storage.close()
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host="0.0.0.0")
